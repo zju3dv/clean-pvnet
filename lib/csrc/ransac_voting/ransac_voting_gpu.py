e2c8c88558f2532bd1861d6a0d6220d558aaf9d2
pengsida <pengsida@zju.edu.cn>
2020-01-02 15:57:08 +0800

    Update ransac_voting_gpu.py

--- lib/csrc/ransac_voting/ransac_voting_gpu.py
+++ lib/csrc/ransac_voting/ransac_voting_gpu.py
@@ -102,7 +102,10 @@ def b_inv(b_mat):
     :return:
     '''
     eye = b_mat.new_ones(b_mat.size(-1)).diag().expand_as(b_mat)
-    b_inv, _ = torch.solve(eye, b_mat)
+    try:
+        b_inv, _ = torch.solve(eye, b_mat)
+    except:
+        b_inv = eye
     return b_inv
 
 
